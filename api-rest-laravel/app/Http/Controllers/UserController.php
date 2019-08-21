ce53c259fc061b8939e3a27c99837e7aea46593f
Carlos Martín <s_martincj@globalhitss.com>
2019-08-21 12:08:27 -0500

    funcion registrar usuarios completa

--- api-rest-laravel/app/Http/Controllers/UserController.php
+++ api-rest-laravel/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -16,17 +17,46 @@ class UserController extends Controller
      */
     public function register(Request $request): JsonResponse
     {
-        dd($request->toArray());
-
-
-
         $data = [
-          'status' => 'error',
-          'code' => 500,
-          'message' => 'El usuario no se registro correctamente'
+            'status' => 'error',
+            'code' => 500,
+            'message' => 'El usuario no se registro correctamente'
         ];
 
+        $validator = Validator::make($request->toArray(), [
+            'name' => ['required', 'string', 'max:50', 'alpha'],
+            'surname' => ['required', 'string', 'max:100', 'alpha'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
+        ]);
 
+        if($validator->fails()) {
+            $data = [
+                'status' => 'error',
+                'code' => 401,
+                'message' => 'El usuario no se registro correctamente',
+                'errors' => $validator->errors()
+            ];
+
+        } else {
+            $inputs = $request->toArray();
+            $inputs['role'] = "ROLE_USER";
+            $inputs['description'] = "Usuario nuevo";
+            $inputs['image'] = "";
+            $inputs['password'] = bcrypt($inputs['password']);
+            unset($inputs['password_confirmation']);
+            $inputs = array_map(
+                'trim', $inputs
+            );
+
+            $user = User::create($inputs);
+            $data = [
+                'status' => 'success',
+                'code' => 200,
+                'message' => 'El usuario se registro correctamente',
+                'user_id' => $user->id
+            ];
+        }
 
         return response()->json($data,$data['code']);
     }
