033a22815f61f7825d57e093477c252afee465c2
Carlos Martín <s_martincj@globalhitss.com>
2019-08-20 19:18:33 -0500

    refinamos rutas necesarios y creamos controladores

---
+++ api-rest-laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Register a user
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Login a user
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        //
+    }
+}
